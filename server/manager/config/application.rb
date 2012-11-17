a30a6ce3818b3743365734daa18ce572123c3bad
freemanoid <freemanoid321@gmail.com>
2012-11-17 20:54:09 +0300

    Fix paths logic.

--- server/manager/config/application.rb
+++ server/manager/config/application.rb
@@ -1,2 +1,2 @@
 # initializers
-Dir["initializers/*.rb"].each {|file| require file }
+Dir[File.expand_path('../initializers/*.rb', __FILE__)].each {|file| require file}
--- server/manager/config/initializers/database.rb
+++ server/manager/config/initializers/database.rb
@@ -1,6 +1,5 @@
-require 'rubygems'
 require 'active_record'
 require 'yaml'
 
-dbconfig = YAML::load(File.open('../database.yml'))
+dbconfig = YAML::load(File.open(File.expand_path('../../database.yml', __FILE__)))
 ActiveRecord::Base.establish_connection(dbconfig)
