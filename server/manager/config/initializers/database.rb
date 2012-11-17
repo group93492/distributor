4fd7489aec166e01f914e2fba98fd510488c81da
freemanoid <freemanoid321@gmail.com>
2012-11-17 23:28:53 +0300

    Add logger for db and remove colors from log to might read log files.

--- server/manager/config/initializers/database.rb
+++ server/manager/config/initializers/database.rb
@@ -1,5 +1,8 @@
 require 'active_record'
 require 'yaml'
+require 'logger'
 
 dbconfig = YAML::load(File.open(File.expand_path('../../database.yml', __FILE__)))
 ActiveRecord::Base.establish_connection(dbconfig)
+ActiveRecord::Base.logger = Logger.new(File.open('log/database.log', 'a'))
+ActiveSupport::LogSubscriber.colorize_logging = false
