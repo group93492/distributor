72c8a82e6f494d4b1cc86d79eaa2f62f26a4a6b2
freemanoid <freemanoid321@gmail.com>
2012-11-19 02:47:11 +0300

    Move all presettings to config/application.

--- server/manager/config/application.rb
+++ server/manager/config/application.rb
@@ -1,2 +1,5 @@
+require 'rubygems'
+require 'bundler/setup'
+
 # initializers
 Dir[File.expand_path('../initializers/*.rb', __FILE__)].each {|file| require file}
--- server/manager/manager.rb
+++ server/manager/manager.rb
@@ -1,7 +1,5 @@
-require 'rubygems'
 require './config/application.rb'
 gem 'activerecord'
-require 'active_record'
 
 class User < ActiveRecord::Base
 end
