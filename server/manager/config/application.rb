a4656b10a1514ea9b1b7c7b950cb98588ab65f7e
ouyoutouchmytralala <freemanoid321@gmail.com>
2012-11-19 19:03:29 +0300

    Require all models from convinient place.

--- server/manager/config/application.rb
+++ server/manager/config/application.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'active_record'
 
 # initializers
 Dir[File.expand_path('../initializers/*.rb', __FILE__)].each {|file| require file}
+# require all models
+Dir[File.join("./app", "**/*.rb")].each {|file| require file}
