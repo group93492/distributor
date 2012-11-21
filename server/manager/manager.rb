41bfb36465a1cf6cb0ed89400beaa4290f748298
freemanoid <freemanoid321@gmail.com>
2012-11-22 01:42:13 +0300

    Add some kind of test version of server to show visibility of work and make other devs happy.

--- server/manager/manager.rb
+++ server/manager/manager.rb
@@ -1,8 +1,54 @@
 require './config/application.rb'
 
-class User < ActiveRecord::Base
+def read_quint8(socket)
+  num = ""
+  1.times {num << socket.getbyte.to_s(16)}
+  num.to_i(16)
 end
 
-# User.create!(:name => 'lol')
+def read_quint16(socket)
+  num = ""
+  2.times {num << socket.getbyte.to_s(16)}
+  num.to_i(16)
+end
 
-p User.all
+def read_quint32(socket)
+  num = ""
+  4.times {num << socket.getbyte.to_s(16)}
+  num.to_i(16)
+end
+
+def read_quint64(socket)
+  num = ""
+  8.times {num << socket.getbyte.to_s(16)}
+  num.to_i(16)
+end
+
+server = TCPServer.new('localhost', 33034)
+
+begin
+  loop do
+    Thread.start(server.accept) do |socket|
+      puts "client #{Addrinfo.new(socket.getpeername).ip_unpack.join(":")} connected"
+      loop do
+        size = read_quint16(socket)
+        p "size: #{size}"
+        type = read_quint8(socket)
+        p "type: #{type}"
+        username_size = read_quint32(socket)
+        p "username_size: #{username_size}"
+        username = ""
+        username_size.times {username << socket.getbyte.to_s(10)}
+        p "username: #{username}"
+        password_size = ""
+        4.times {password_size << socket.getbyte.to_s(16)}
+        p "password_size: #{password_size.to_i(16)}"
+        password = ""
+        password_size.to_i(16).times {password << socket.getbyte.to_s(10)}
+        p "password: #{password}"
+      end
+    end
+  end
+ensure
+  server.close
+end
