b964ac59204426a220e2aabe540a11774bc67e4d
freemanoid <freemanoid321@gmail.com>
2012-11-22 01:40:46 +0300

    Add qtbindings gem and requires.

--- server/manager/config/application.rb
+++ server/manager/config/application.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'active_record'
 require 'socket'
+require 'Qt4'
 
 # initializers
 Dir[File.expand_path('../initializers/*.rb', __FILE__)].each {|file| require file}
