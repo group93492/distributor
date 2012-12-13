511980573cbaa9746d2c515eeb655b565f43b87c
freemanoid <freemanoid321@gmail.com>
2012-12-13 20:51:33 +0300

    Add validators to User model and implement user registration.

--- server/manager/app/models/User.rb
+++ server/manager/app/models/User.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :folders
+  attr_accessible :login, :encrypted_password, :rights
+  # attr_accessor :socket
+  validates_uniqueness_of :login, :message => "must be unique"
+  validates_presence_of :login, :message => "can't be blank"
+  validates_presence_of :encrypted_password, :message => "can't be blank"
+
+  def self.register_user(registration_request)
+    User.create(:login => registration_request.username,
+                :encrypted_password => registration_request.password)
+  end
 end
--- server/manager/manager.rb
+++ server/manager/manager.rb
@@ -13,24 +13,36 @@ require './config/application.rb'
 
 server = TCPServer.new(SERVER_IP, SERVER_PORT)
 
+def confirm_registration(socket)
+  socket.send_packet({'type' => 'registration_answer',
+                       'registration_result' => true})
+end
+
+def reject_registration(socket, denial_reason = "")
+  socket.send_packet({'type' => 'registration_answer',
+                      'registration_result' => false,
+                      'denial_reason' => denial_reason})
+end
+
 begin
   puts "Server started on #{server.connect_address.ip_unpack.join(":")}"
   loop do
     Thread.start(server.accept) do |socket|
       puts "client #{Addrinfo.new(socket.getpeername).ip_unpack.join(":")} connected"
-      p socket.get_packet
       loop do
-        # size = read_uint16(socket)
-        # p "size: #{size}"
-        # type = read_uint8(socket)
-        # p "type: #{type}"
-        # username = ""
-        # username = read_qbytearray(socket)
-        # p "username: #{username}"
-        # password = read_qbytearray(socket)
-        # p "password: #{password}"
-
-        # write_registration_denied(socket)
+        current_packet = socket.get_packet
+        case current_packet.type
+        when 'unknown'
+          print "Got unknown-typed packet #{current_packet}\n"
+        when 'authorization_request'
+        when 'registration_request'
+          user = User.register_user(current_packet)
+          if user.valid?
+            confirm_registration(socket)
+          else
+            reject_registration(socket, user.errors.messages.to_s)
+          end
+        end
       end
     end
   end
