65618f4419b5ac14f1e7313c35c3e7ceeea63d7c
freemanoid <freemanoid321@gmail.com>
2012-12-13 23:39:14 +0300

    Add socket and online attributes for User.

--- server/manager/app/models/User.rb
+++ server/manager/app/models/User.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :folders
   attr_accessible :login, :encrypted_password, :rights
-  # attr_accessor :socket
+  attr_accessor :online, :socket
   validates_uniqueness_of :login, :message => "must be unique"
   validates_presence_of :login, :message => "can't be blank"
   validates_presence_of :encrypted_password, :message => "can't be blank"
